a371bb2ea8960b0cff2fe472dfffcd9ea5b239fe
Adarsh Hiwrale <ahiwrale@sigmasolve.net>
2023-07-27 22:22:44 +0530

    providers

--- versions.tf
+++ versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0"
+      version = "> 2.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 2.1"
+      version = "> 2.1"
     }
   }
 }
